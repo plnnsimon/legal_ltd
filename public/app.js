da0bb7255887899ed6f5e0a56210deb09a1aac3e
Олександр Пальонний <s2567k@gmail.com>
2023-10-03 22:32:22 +0300

    added listeners for buttons for g-tag

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 require('./scripts/email.js')
+require('./scripts/g-tag-event.js')
 require('./scripts/get-last-day-of-month.js')
 require('./scripts/validation/phone-validation.js')
 require('./scripts/get-time-remainig.js')
---
+++ public/scripts/g-tag-event.js
@@ -0,0 +1,24 @@
+const buttonsToFollow = document.getElementsByClassName('g-tag-event')
+
+for (const button of buttonsToFollow) {
+  button.addEventListener('click', gtagReportConversion)
+}
+
+
+function gtagReportConversion(url) {
+  try {
+    const callback = () => {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+  
+    gtag('event', 'conversion', {
+        'send_to': 'AW-10980539371/M4U6CLL9nOgYEOv39vMo',
+        'event_callback': callback
+    });
+    return false;
+  } catch (err) {
+    console.error(err);
+  }
+}

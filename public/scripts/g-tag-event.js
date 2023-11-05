7db30e4f19526adfba735080c22d9e2d2b421292
Олександр Пальонний <s2567k@gmail.com>
2023-11-05 22:05:01 +0200

    update gtag event

--- public/scripts/g-tag-event.js
+++ public/scripts/g-tag-event.js
@@ -1,23 +1,23 @@
-// const buttonsToFollow = document.getElementsByClassName('g-tag-event')
+const buttonsToFollow = document.getElementsByClassName('g-tag-event')
 
-// for (const button of buttonsToFollow) {
-//   button.addEventListener('click', gtagReportConversion)
-// }
+for (const button of buttonsToFollow) {
+  button.addEventListener('click', gtagReportConversion)
+}
 
 
 function gtagReportConversion(url) {
   try {
     const callback = () => {
-      if (typeof(url) != 'undefined') {
-        window.location = url;
-      }
+      // if (typeof(url) != 'undefined') {
+      //   window.location = url;
+      // }
+      console.log('Gtag event sent');
     };
   
     gtag('event', 'conversion', {
         'send_to': 'AW-10980539371/M4U6CLL9nOgYEOv39vMo',
         'event_callback': callback
     });
-    console.log('event sent');
     return false;
   } catch (err) {
     console.error(err);
